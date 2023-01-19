70a2ca1ddff403d67b32ad4274bca666190953c1
Liioan <dev.davidbijok@gmail.com>
2023-01-20 00:54:06 +0100

    add attribution

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ function App() {
   return (
     <div className='App'>
       <Card />
+      <div class='attribution'>
+        Challenge by{' '}
+        <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by <a href='https://github.com/liioan'>David Bijok</a>.
+      </div>
     </div>
   );
 }
