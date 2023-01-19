a4d8ec6cade05ea9786e44bf0a7d90249cfd8984
Liioan <dev.davidbijok@gmail.com>
2023-01-19 21:08:08 +0100

    add fetch advice functionality

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
 //. styles
 import styles from './Card.module.css';
 
@@ -7,17 +10,52 @@ import MobileDevider from '../../assets/mobileDevider.svg';
 import Dice from '../../assets/dice.svg';
 
 export default function Card() {
+  const [data, setData] = useState({ advice: '', id: '' });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const json = await res.json();
+    setData(json.slip);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleClick = () => {
+    if (isLoading) return;
+    setIsLoading(true);
+    setTimeout(() => {
+      fetchData();
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
     <main className={styles.card}>
       <div className={styles.advice}>
-        <span>advice number</span>
-        <p>
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempora
-          placeat officia nulla numquam. Ipsam quidem nostrum neque veniam non?"
-        </p>
+        <span>advice # {data.id}</span>
+        <AnimatePresence>
+          {!isLoading && (
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1, ease: 'anticipate' }}
+            >
+              <span className='material-symbols-outlined'>format_quote</span>
+              <span className={styles.adviceText}>{data.advice}</span>
+              <span className='material-symbols-outlined'>format_quote</span>
+            </motion.p>
+          )}
+        </AnimatePresence>
       </div>
       <img src={DesktopDevider} alt='' />
-      <button>
+      <button
+        onClick={handleClick}
+        className={isLoading ? styles.disabled : ''}
+      >
         <img src={Dice} alt='' />
       </button>
     </main>
